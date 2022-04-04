e0ca6328ab787c4da5cfd85e3a92916c869d97f2
Nico Velazquez <nicovege@gmail.com>
2022-04-04 03:21:50 -0300

    cart y itemlist

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import NavBar from './Components/NavBar/NavBar';
-import ItemListener  from './Components/ItemListContainer/ItemListContainer';
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import ItemListener  from "./Components/ItemListContainer/ItemListContainer";
 
 //IMPORTANTE: <P></P> & <p></p> => no son lo mismo, el primero es una variable y el segundo es una etiqueta html
 const App = () =>{
 
   return(
-
+    <>
     <div className="App">
       <NavBar/>
-      <ItemListener greeting='Bievenido a Nuestra Tienda' />
+      <ItemListener greeting="Bievenido a Nuestra Tienda" />
     </div>
-
+    </>
   );
 };
 
