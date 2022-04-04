ec2f1bae9255efd7f70015860b3a168ed40e2998
Nico Velazquez <nicovege@gmail.com>
2022-04-04 02:00:02 -0300

    cart y itemlist

--- src/Components/NavBar/CartWidget/CartWidget.js
+++ src/Components/NavBar/CartWidget/CartWidget.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './CartWidget.css';
 
 const CartWidget = () => {
     return (
         <>
         <div>
             <Button variant="secondary" size="sm">
-                <img src="./img/cart.png" alt="cart" />
+                <img src="./img/cart.png" alt="cart" style= {{width: "30px"}}/>
             </Button>
         </div>
         </>
