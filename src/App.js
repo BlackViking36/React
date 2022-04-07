c4d5262584ca97ccd0306756fa40bcf741af9707
Nico Velazquez <nicovege@gmail.com>
2022-04-07 06:58:29 -0300

    useState/ItemCount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import ItemListener  from "./Components/ItemListContainer/ItemListContainer";
+import ItemCount from "./Components/ItemCount/ItemCount";
 
 //IMPORTANTE: <P></P> & <p></p> => no son lo mismo, el primero es una variable y el segundo es una etiqueta html
 const App = () =>{
@@ -10,6 +11,7 @@ const App = () =>{
     <div className="App">
       <NavBar/>
       <ItemListener greeting="Bievenido a Nuestra Tienda" />
+      <ItemCount />
     </div>
     </>
   );
