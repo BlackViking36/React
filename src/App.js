66d4aef5011d4cdf8eee46b245779f51055793dc
Nico Velazquez <nicovege@gmail.com>
2022-04-13 02:20:28 -0300

    lista de gorras

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import ItemListContainer  from "./Components/ItemListContainer/ItemListContainer";
-import ItemCount from "./Components/ItemCount/ItemCount";
 import ItemList from "./Components/ItemListContainer/ItemList";
 
 //IMPORTANTE: <P></P> & <p></p> => no son lo mismo, el primero es una variable y el segundo es una etiqueta html
@@ -12,7 +11,6 @@ const App = () =>{
       <div className="App">
         <NavBar/>
         <ItemListContainer greeting="Caps de calidad" />
-        <ItemCount />
         <ItemList />
       </div>
     </>
